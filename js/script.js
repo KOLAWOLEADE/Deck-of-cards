58ae277d349b6cb44a2b789bc7e9bbbfe61822ed
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-08-14 10:45:55 +0100

    add DECK-OF-CARDS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    let suits = ["H", "C", "D", "S"];
+    let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
+    
+    for (let suitCounter =0; suitCounter < 4; suitCounter ++) {
+        // console.log(suits[suitCounter])
+        for (let rankCounter = 0; rankCounter < 13; rankCounter ++) {
+            console.log(ranks[rankCounter] + suits[suitCounter]);
+        }
+    }
+})
